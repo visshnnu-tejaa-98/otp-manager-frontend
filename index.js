4ce5ffed1f14aaa17e5a706f26e0fc737c7f189a
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-04-20 12:26:12 +0530

    code modified

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 module.exports = {
-	generateOTP: (EMAIL) => {
+	generateOTP: async (EMAIL) => {
+		console.log(EMAIL);
 		const BACKEND_ENDPOINT = 'https://my-otp-manager.herokuapp.com';
 		let response;
-		// const [email, setEmail] = useState(EMAIL);
-		const data = { EMAIL };
-		const postData = async () => {
-			const req = await fetch(`${BACKEND_ENDPOINT}/generate`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data),
-			});
-			const res = await req.json();
-			console.log(res);
-			response = res;
-		};
-		postData();
-		return response;
+		const data = { email: EMAIL };
+		const req = await fetch(`${BACKEND_ENDPOINT}/generate`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+		const res = await req.json();
+		return res;
 	},
-	verifyOTP: (EMAIL, OTP) => {
+	verifyOTP: async (EMAIL, OTP) => {
 		const BACKEND_ENDPOINT = 'https://my-otp-manager.herokuapp.com';
 		let response;
-		let data = { EMAIL, OTP };
+		let data = { email: EMAIL, otp: OTP };
 		console.log(data);
-		const getData = async () => {
-			const req = await fetch(`${BACKEND_ENDPOINT}/verify`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data),
-			});
-			const res = await req.json();
-			console.log(res);
-			return res;
-		};
-		getData();
-		return response;
+		const req = await fetch(`${BACKEND_ENDPOINT}/verify`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+		const res = await req.json();
+		console.log(res);
+		return res;
 	},
 };
