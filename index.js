20b01f246df08447e8f6d526a492c3d81a3e4928
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-04-20 10:38:28 +0530

    code modified

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-export const GenerateOTP = (EMAIL) => {
-	const BACKEND_ENDPOINT = 'https://my-otp-manager.herokuapp.com';
-	const [email, setEmail] = useState(EMAIL);
-	let data = { email };
-	console.log(data);
-	const postData = async () => {
-		const req = await fetch(`${BACKEND_ENDPOINT}/generate`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		});
-		const res = await req.json();
-		console.log(res);
-		return res;
-	};
-	postData();
-};
-
-export const VerifyOTP = (EMAIL, OTP) => {
-	const BACKEND_ENDPOINT = 'https://my-otp-manager.herokuapp.com';
-	const [email, setEmail] = useState(EMAIL);
-	const [otp, setOpt] = useState(OTP);
-	let data = { email, otp };
-	console.log(data);
-	const getData = async () => {
-		const req = await fetch(`${BACKEND_ENDPOINT}/verify`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		});
-		const res = await req.json();
-		console.log(res);
-		return res;
-	};
-	getData();
+module.exports = {
+	generateOTP: (EMAIL) => {
+		const BACKEND_ENDPOINT = 'https://my-otp-manager.herokuapp.com';
+		const response;
+		// const [email, setEmail] = useState(EMAIL);
+		const data = { EMAIL };
+		const postData = async () => {
+			const req = await fetch(`${BACKEND_ENDPOINT}/generate`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
+			const res = await req.json();
+			console.log(res);
+			response = res;
+		};
+		postData();
+		return response;
+	},
+	verifyOTP: (EMAIL, OTP) => {
+		const BACKEND_ENDPOINT = 'https://my-otp-manager.herokuapp.com';
+		let data = { EMAIL, OTP };
+		console.log(data);
+		const getData = async () => {
+			const req = await fetch(`${BACKEND_ENDPOINT}/verify`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
+			const res = await req.json();
+			console.log(res);
+			return res;
+		};
+		getData();
+	},
 };
